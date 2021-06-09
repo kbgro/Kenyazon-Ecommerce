c94ebaf689f36cc2b1d770cffa351b0d1e9bb9d0
Michael Macharia Ngigi <michael.ngigi@strathmore.edu>
2021-06-09 23:19:39 +0300

    Improved code on Login/Register Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,57 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from accounts.forms import RegistrationForm, LoginForm
+from accounts.models import Account
 
 
 # Create your views here.
+
+
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid login credentials🤷‍')
+            return redirect('login')
+
     return render(request, template_name='accounts/login.html')
 
 
 def register(request):
-    return render(request, template_name='accounts/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
 
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name,
+                                               email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'User registered successfully😎')
+            return redirect('register')
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, template_name='accounts/register.html', context=context)
+
+
+@login_required(login_url='login')
 def logout(request):
-    return 
+    auth.logout(request)
+    messages.success(request, 'You\'re logged out👋🏻')
+    return redirect('login')
--- kenyazon/settings.py
+++ kenyazon/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,7 +28,6 @@ SECRET_KEY = 'django-insecure-(mci4s@ju5ewejnvqaj*tw%bt49l%f4h4#5jtl1yx@*t^(k!t0
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -82,7 +81,6 @@ WSGI_APPLICATION = 'kenyazon.wsgi.application'
 
 AUTH_USER_MODEL = 'accounts.Account'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -92,7 +90,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -112,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -125,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -141,3 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media Files Configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
